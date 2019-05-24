9e1a675ae64daae0cca708d0e2124ffea42b4fb6
Alexandra <sgrjaznova@gmail.com>
2019-05-24 10:17:52 +0300

    Modified User tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,15 +8,25 @@ RSpec.describe User, type: :model do
       expect(User.all.count).to eq(1)
     end
 
-    it 'password cant be longer than 5 characters' do
+    it 'password cant be shorter than 5 characters' do
       user = User.create!(login: 'sasha', password:'12345')
       expect(user.valid?).to eq(true)
     end
 
     it 'checks if login is unique' do
       user1 = User.create!(login: 'sasha', password: '12345')
-      user2 = User.create!(login: 'sasha', password: '54321')
+      user2 = User.new(login: 'sasha', password: '54321')
       expect(user2.valid?).to eq(false)
+    end
+
+    it 'checks if login is present' do
+      user = User.new(login: '', password: '123456')
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'checks if password is present' do
+      user = User.new(login: 'sashaaa', password: '')
+       expect(user.valid?).to eq(false)
     end
 
   end
